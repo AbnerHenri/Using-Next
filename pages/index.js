103d42d2dc9b9ee325ca31029e19f192bbc0783d
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2022-06-14 20:12:42 -0300

    Reorganizing Project

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,37 @@ import styles from '../styles/Home.module.css'
 
 import Head from 'next/head'
 
-export default function Home() {
+export async function getStaticProps() {
+
+  const data = await fetch('https://rickandmortyapi.com/api/character')
+  const character = await data.json()
+  const results = await character.results
+
+  return{
+    props : { results }
+  }
+}
+
+export default function Home({ results }) {
+
+    function checkStatus(status){
+      switch (status) {
+      case 'Alive':
+        return  { color : 'green' }
+      
+      
+      case 'unknown' :
+        return  { color : 'gray' }
+
+
+      case 'Dead' : 
+        return  { color : 'red' }
+    
+      default:
+        break;
+    }
+  }
+
   return (
     <div>
       <Head>
@@ -11,7 +41,10 @@ export default function Home() {
         <meta name='description' content='Encontre as melhores roupas pra você' />
       </Head>
 
-        
+      <div style={{ marginLeft : 15 }}>
+        <h1>Página Teste</h1>
+        {results.map((e)=> <h4>{e.name} - <span style={checkStatus(e.status)}>{e.status}</span></h4>)}
+      </div>
 
     </div>
   )
